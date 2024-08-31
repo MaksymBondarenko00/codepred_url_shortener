b23b1d3debb2d2d1364057a4b70d76f3bf5d2831
BondarenkoMM <masya.klas3000@gmail.com>
2024-08-31 05:33:41 +0200

    test commit

---
+++ src/main/java/com/cpr/codepred_url_shortener/CodepredUrlShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.cpr.codepred_url_shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodepredUrlShortenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodepredUrlShortenerApplication.class, args);
+    }
+
+}
